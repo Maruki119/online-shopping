7d25a2118c78ac197f3c6d5f6fbeb157ed19bf0a
Maruki119 <93820527+Maruki119@users.noreply.github.com>
2024-02-06 02:41:16 +0700

    Change server to main API add mongodb to server add all collection add greeting func add get_all_users func

--- backend/server.py
+++ backend/server.py
@@ -1,24 +1,29 @@
-from flask import Flask
-import datetime
+from flask import Flask, jsonify, request
+from pymongo.mongo_client import MongoClient
+
+#add mongodb to api
+uri = "mongodb+srv://user:user@online-shopping.uyqkvxp.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(uri)
+#add client online-shopping = db
+db = client["online-shopping"]
+#add all collection
+users_collection = db["users"]
+items_collection = db["items"]
+cards_collection = db["cards"]
  
-x = datetime.datetime.now()
- 
+#main app
 app = Flask(__name__)
  
-@app.route('/api/data')
-def get_time():
-    return {
-        'Name':"geek",
-        "Age":"22",
-        "Date":x, 
-        "programming":"python"
-        }
- 
+#greeting api
 @app.route('/api')
-def get_specific():
-    return {
-        'Name' : "Nititorn"
-    }
+def Greet():
+    return "<p>Welcome to Online-Shopping Management API</p>"
+
+@app.route('/api/users', methods = ['GET'])
+def get_all_users():
+    all_users = users_collection.find()
+    return jsonify({"users":[i for i in all_users]})
      
 if __name__=="__main__":
     app.run(host = "0.0.0.0", port = 5000, debug = True)
