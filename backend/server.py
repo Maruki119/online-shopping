22bede29ab4de3bc22ebe9ccfae4f95532eb79b9
Maruki119 <93820527+Maruki119@users.noreply.github.com>
2024-02-06 03:08:41 +0700

    Add more user func add get_user func add create_user func add update_user func

--- backend/server.py
+++ backend/server.py
@@ -20,10 +20,48 @@ app = Flask(__name__)
 def Greet():
     return "<p>Welcome to Online-Shopping Management API</p>"
 
-@app.route('/api/users', methods = ['GET'])
+#users
+@app.route('/api/users', methods = ["GET"])
 def get_all_users():
     all_users = users_collection.find()
     return jsonify({"users":[i for i in all_users]})
+
+@app.route("/api/users/<int:user_id>", methods = ["GET"])
+def get_user(user_id):
+    all_users = users_collection.find()
+    user = next( (i for i in all_users if i["_id"] == user_id), None)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"}), 404
      
+@app.route("/api/users", methods = ["POST"])
+def create_user():
+    try:
+        data = request.get_json()
+        new_user = {
+            "_id": users_collection.count_documents({}) + 1,
+            "user": data["user"],
+            "password": data["password"],
+            "email": data["email"],
+            "fullname": data["fullname"],
+            "balance": 0
+        }
+        users_collection.insert_one(new_user)
+        return jsonify(new_user), 200
+    except Exception as e:
+        return jsonify({"error":"Cannot create new user"}), 500     
+     
+@app.route("/api/users/<int:user_id>", methods = ["PUT"])
+def update_user(user_id):
+    all_users = users_collection.find()
+    user = next( (i for i in all_users if i["_id"] == user_id), None)
+    if user:
+        data = request.get_json()
+        users_collection.update_one({"_id": user_id}, {"$set": data})
+        return jsonify(user), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
+    
 if __name__=="__main__":
     app.run(host = "0.0.0.0", port = 5000, debug = True)
