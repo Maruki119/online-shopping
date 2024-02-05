088401bca3d168e43b016dac4680e1db2b308595
Maruki119 <93820527+Maruki119@users.noreply.github.com>
2024-02-06 03:47:20 +0700

    Add Card API and fixed func add get_all_cards func add get_card func add create_card func add update_card func add delete_card func fixed create_user func

--- backend/server.py
+++ backend/server.py
@@ -45,10 +45,11 @@ def create_user():
             "password": data["password"],
             "email": data["email"],
             "fullname": data["fullname"],
+            "card_id": "",
             "balance": 0
         }
         all_users = list(users_collection.find())
-        if(next((s for s in all_users if s["user"] == data["user"]), None)):
+        if(next((i for i in all_users if i["user"] == data["user"]), None)):
             return jsonify( {"error":"Cannot create new user"}), 500
         else:
             users_collection.insert_one(new_user)
@@ -66,6 +67,61 @@ def update_user(user_id):
         return jsonify(user), 200
     else:
         return jsonify({"error": "User not found"}), 404
+
+#cards
+@app.route("/api/cards", methods = ["GET"])
+def get_all_cards():
+    all_cards = cards_collection.find()
+    return jsonify({"cards":[i for i in all_cards]})
+
+@app.route("/api/cards/<int:card_id>", methods = ["GET"])
+def get_card(card_id):
+    all_cards = cards_collection.find()
+    card = next( (i for i in all_cards if i["_id"] == card_id), None)
+    if card:
+        return jsonify(card)
+    else:
+        return jsonify({"error": "Card not found"}), 404
+
+@app.route("/api/cards", methods = ["POST"])
+def create_card():
+    try:
+        data = request.get_json()
+        new_card = {
+            "_id": cards_collection.count_documents({}) + 1,
+            "number": data["number"],
+            "holder": data["holder"],
+            "balance": data["balance"]
+        }
+        all_cards = list(cards_collection.find())
+        if(next((i for i in all_cards if i["number"] == data["number"]), None)):
+            return jsonify( {"error":"Cannot create new card"}), 500
+        else:
+            cards_collection.insert_one(new_card)
+            return jsonify(new_card),200
+    except Exception as e:
+        print(e)
+     
+@app.route("/api/cards/<int:card_id>", methods = ["PUT"])
+def update_card(card_id):
+    all_cards = cards_collection.find()
+    card = next( (i for i in all_cards if i["_id"] == card_id), None)
+    if card:
+        data = request.get_json()
+        cards_collection.update_one({"_id": card_id}, {"$set": data})
+        return jsonify(card), 200
+    else:
+        return jsonify({"error": "Card not found"}), 404
+    
+@app.route("/api/cards/<int:card_id>", methods = ["DELETE"])
+def delete_card(card_id):
+    all_cards = cards_collection.find()
+    card = next( (i for i in all_cards if i["_id"] == card_id), None)
+    if card:
+        cards_collection.delete_one({"_id": card_id})
+        return jsonify({"message": "Card deleted successfully"}), 200
+    else:
+        return jsonify({"error": "Card not found"}), 404
     
 if __name__=="__main__":
     app.run(host = "0.0.0.0", port = 5000, debug = True)
