a5a539c51f932fa01e76e780723ff2752b695e45
phuriphat20 <155616139+phuriphat20@users.noreply.github.com>
2024-02-08 21:04:05 +0700

    create Navbar

---
+++ frontend/src/Components/Navbar.js
@@ -0,0 +1,20 @@
+    import './Navbar.css'
+
+    function Navbar()
+    {
+        return (
+            <div className="App-header">
+                <div className = "container">
+                    <div className="App-logo">
+
+                    </div>
+                    <div className="App-name">
+                        <h1>ข้าวกล่อง</h1>
+                            <h2>E-Books</h2>
+                    </div>
+                </div>   
+            </div>
+        );    
+    }
+
+    export default Navbar;
