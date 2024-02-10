10b059810ebf1eed62abb0606f8ac7dc6d65bb1f
phuriphat20 <155616139+phuriphat20@users.noreply.github.com>
2024-02-10 23:28:59 +0700

    Navbar non event

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -3,16 +3,29 @@
     function Navbar()
     {
         return (
-            <div className="App-header">
-                <div className = "container">
-                    <div className="App-logo">
+            <div className="Navbar">
+                
+                <div className="logo">
+                    <img className='App-logo' src = '/images/e-book.png'/>
+                </div>
+                <div className='menu'>
+                    <img className='App-menu' src = '/images/menu-burger.png'/>
+                    <h1>Menu</h1>
+                </div>
+                <div className="App-search">
+                    <input
+                    className='serch-input'
+                    type='text'
+                    placeholder="วันนี้อ่านอะไรดีจ้ะ?" 
+                />
+                </div>
+                <div className = "Sign-in">
+                    <button className="sign-in-button">เข้าสู่ระบบ</button>
+                </div>
 
-                    </div>
-                    <div className="App-name">
-                        <h1>ข้าวกล่อง</h1>
-                            <h2>E-Books</h2>
-                    </div>
-                </div>   
+                <div className = "Sign-up">
+                    <button className="sign-up-button">สมัครสมาชิก</button>
+                </div>
             </div>
         );    
     }
