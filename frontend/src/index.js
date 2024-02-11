25afe2317439d1c13648310c197175dcc1519734
Sasikhet <Sasikhet.15272@gmail.com>
2024-02-11 22:47:13 +0700

    login-002

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './login.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import LoginForm from './login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LoginForm />
   </React.StrictMode>
 );
 
--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import loginLogo from "./images/Khaoklong.png";
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -10,19 +10,20 @@ const LoginForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="login-form">
+    <div className="login-container">
+      <img className='login-logo' src={loginLogo} alt="Login Logo" />
       <h2 className="login-title">ยินดีต้อนรับ เข้าสู่ระบบ!</h2>
       <p className = "login-subtitle">หากมีบัญชีแล้ว สามารถเข้าสู่ระบบด้วยบัญชีเดิมได้เลย</p>
       <input
         type="email"
-        placeholder="Email or username"
+        placeholder="อีเมลหรือชื่อผู้ใช้"
         className="login-input"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
       <input
         type="password"
-        placeholder="Password"
+        placeholder="รหัสผ่าน"
         className="login-input"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
@@ -30,10 +31,10 @@ const LoginForm = () => {
       <button type="submit" className="login-button">
         เข้าสู่ระบบ
       </button>
-      <div class="horizontal-line">
-        <div class="line"></div>
-        <span class="text-right">หรือ</span>
-        <div class="line"></div>
+      <div className="horizontal-line">
+        <div className="line"></div>
+        <div className="text-right">หรือ</div>
+        <div className="line"></div>
       </div>
       <button type="submit" className="apple-button">
         เข้าสู่ระบบด้วย Apple ID
@@ -46,7 +47,7 @@ const LoginForm = () => {
           <u>สมัครสมาชิก</u>
         </a>
       </p>
-    </form>
+    </div>
   );
 };
 
